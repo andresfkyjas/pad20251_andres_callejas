397bd21b4ef62a209111ba4c4aa8a59b46d5aecf
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-02-27 00:50:01 +0000

    proyecto cargado

--- setup.py
+++ setup.py
@@ -12,4 +12,4 @@ setup(
         "openpyxl",
         "requests"
     ]
-    
+) 
--- src/ibgd/ingestion.py
+++ src/ibgd/ingestion.py
@@ -1,24 +1,34 @@
 import requests
 import json
 
+class Ingestion:
+    def __init__(self):
+        self.ruta_static = "src/ibgd/static/"
 
-
-def obtener_datos_api(url="",params={}):
-    url = "{}/{}/{}/".format(url,params["coin"],params["method"])
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as error:
-        print(error)
-        return {}
+    def obtener_datos_api(self,url="",params={}):
+        url = "{}/{}/{}/".format(url,params["coin"],params["method"])
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(error)
+            return {}
     
-
+    def guardar_datos(self,datos={},nombre_archivo="ingestion"):
+        with open("{}db/{}.json".format(self.ruta_static,nombre_archivo), "w") as archivo:
+            json.dump(datos,archivo)
+        
+    def guardar_db(self,datos={},nombre_archivo="ingestion"):
+        pass
+    
+ingestion = Ingestion()
 parametros = {"coin":"BTC","method":"ticker"}
 url = "https://www.mercadobitcoin.net/api"
-datos = obtener_datos_api(url=url, params=parametros)
-
+datos = ingestion.obtener_datos_api(url=url, params=parametros)
 if len(datos)>0:
     print(json.dumps(datos,indent=4))
 else:
     print("no se obtubo la consulta")
+ingestion.guardar_datos(datos=datos,nombre_archivo="ingestion")
+
